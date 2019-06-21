daf26ca24074db242ccbfabb27bc31370cdf218c
c-tel <stas.telizhenko@gmail.com>
2019-06-22 00:30:58 +0300

    add file to Project

--- Dima/Database/Entities/Project.cs
+++ Dima/Database/Entities/Project.cs
@@ -6,5 +6,6 @@ namespace Dima.Database.Entities
         public int Id_Project { get; set; }
         public string Request_Name { get; set; }
         public bool Status { get; set; }
+        public byte[] ProjFile {  get; set; }
     }
 }

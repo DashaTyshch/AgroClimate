9d6826e7a36d2c325b1d8fe76a5ac69193661684
dashatyshch <dasha.tyshenko98@gmail.com>
2019-06-21 22:48:45 +0300

    Added Storage

---
+++ Dima/Models/Storage.cs
@@ -0,0 +1,29 @@
+﻿using Dima.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Models
+{
+    public class Storage
+    {
+        private static Storage _instance;
+
+        public Request SelectingRequest { get; private set; }
+
+        private static Storage GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Storage();
+            return _instance;
+        }
+
+        public void ChangeSelectedRequest(Request request)
+        {
+            SelectingRequest = request;
+        }
+
+    }
+}
